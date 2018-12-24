c94f36f235644663cc6512f20821189e047c92b1
kkyusik <wpkyusik@gmail.com>
2018-12-24 18:40:22 -0500

    modify codes

--- R/IndVariety-package.R
+++ R/IndVariety-package.R
@@ -1,3 +1,11 @@
 #' @importFrom tibble tibble
+#' @importFrom dplyr mutate
+#' @importFrom dplyr summarise
+#' @importFrom dplyr group_by
+#' @importFrom dplyr left_join
+#' @importFrom dplyr select
+#' @importFrom dplyr rename
+#' @importFrom magrittr "%>%"
+#'
 #' @keywords internal
 "_PACKAGE"
