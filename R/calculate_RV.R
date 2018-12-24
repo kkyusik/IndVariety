9373b38c4aae4dbfec4a817b38550b9be8dad3d4
kkyusik <wpkyusik@gmail.com>
2018-12-24 18:57:50 -0500

    modify codes

--- R/calculate_RV.R
+++ R/calculate_RV.R
@@ -3,7 +3,6 @@
 #' This function allows you to calculate Related Variety index based on 2015 administration.
 #' @param data Put your Korean Business Survey data dataframe.
 #' @param year Put `year` you want to calculate.
-#'
 #' @examples
 #' data <- read.table("KoreanBusinessSurvey.txt", sep = "\t", colClasses = "character")
 #' RV(data = data, year = 2013)
